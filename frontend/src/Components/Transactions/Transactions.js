e34bdecaac811ec9bcf69f200695b5eeecafaa0c
BhaskarAcharjee <bhaskaracharjee2002@gmail.com>
2023-08-19 10:33:17 +0530

    Implemented Search Sort Filter functionality

--- frontend/src/Components/Transactions/Transactions.js
+++ frontend/src/Components/Transactions/Transactions.js
@@ -1,67 +1,130 @@
-import React, { useEffect } from 'react'
-import styled from 'styled-components';
-import { InnerLayout } from '../../styles/Layouts';
-import { useGlobalContext } from '../../context/globalContext';
-import IncomeItem from '../IncomeItem/IncomeItem';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { InnerLayout } from "../../styles/Layouts";
+import { useGlobalContext } from "../../context/globalContext";
+import IncomeItem from "../IncomeItem/IncomeItem";
 
 function Transactions() {
-    const { incomes, expenses, getIncomes, getExpenses, deleteIncome, deleteExpense } = useGlobalContext();
+  const {
+    incomes,
+    expenses,
+    getIncomes,
+    getExpenses,
+    deleteIncome,
+    deleteExpense,
+  } = useGlobalContext();
+  const [sortType, setSortType] = useState("date");
+  const [filterType, setFilterType] = useState("all");
+  const [searchTerm, setSearchTerm] = useState("");
 
-    useEffect(() =>{
-        getIncomes()
-        getExpenses()
-    }, [])
-    return (
-        <TransactionsStyled>
-            <InnerLayout>
-                <h1>All Transactions</h1>
-                <div className="transactions-content">
-                    <div className="transactions">
-                        {incomes.map((income) => (
-                            <IncomeItem
-                                key={income._id}
-                                id={income._id}
-                                title={income.title}
-                                description={income.description}
-                                amount={income.amount}
-                                date={income.date}
-                                type={income.type}
-                                category={income.category}
-                                indicatorColor="var(--color-green)"
-                                deleteItem={deleteIncome}
-                            />
-                        ))}
-                        {expenses.map((expense) => (
-                            <IncomeItem
-                                key={expense._id}
-                                id={expense._id}
-                                title={expense.title}
-                                description={expense.description}
-                                amount={expense.amount}
-                                date={expense.date}
-                                type={expense.type}
-                                category={expense.category}
-                                indicatorColor="red"
-                                deleteItem={deleteExpense}
-                            />
-                        ))}
-                    </div>
-                </div>
-            </InnerLayout>
-        </TransactionsStyled>
+  useEffect(() => {
+    getIncomes();
+    getExpenses();
+  }, []);
+
+  // Combine all transactions and sort them according to the selected sort type
+  const allTransactions = [...incomes, ...expenses];
+  allTransactions.sort((a, b) => {
+    if (sortType === "date") {
+      return new Date(b.date) - new Date(a.date);
+    } else if (sortType === "amount") {
+      return b.amount - a.amount;
+    }
+  });
+
+  const filteredTransactions =
+    filterType === "all"
+      ? allTransactions
+      : allTransactions.filter((item) => item.type === filterType);
+
+  const searchFilteredTransactions = searchAndFilter(
+    searchTerm,
+    filteredTransactions
+  );
+
+  function searchAndFilter(searchTerm, data) {
+    return data.filter((item) =>
+      item.title.toLowerCase().includes(searchTerm.toLowerCase())
     );
+  }
+
+  return (
+    <TransactionsStyled>
+      <InnerLayout>
+        <h1>All Transactions</h1>
+        <div className="transactions-content">
+          <div className="sorting">
+            <select
+              value={sortType}
+              onChange={(e) => setSortType(e.target.value)}
+            >
+              <option value="date">Sort by Date</option>
+              <option value="amount">Sort by Amount</option>
+            </select>
+            <select
+              value={filterType}
+              onChange={(e) => setFilterType(e.target.value)}
+            >
+              <option value="all">All</option>
+              <option value="income">Income</option>
+              <option value="expense">Expense</option>
+            </select>
+            <input
+              type="text"
+              placeholder="Search"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
+          <div className="transactions">
+            {searchFilteredTransactions.map((transaction) => (
+              <IncomeItem
+                key={transaction._id}
+                id={transaction._id}
+                title={transaction.title}
+                description={transaction.description}
+                amount={transaction.amount}
+                date={transaction.date}
+                type={transaction.type}
+                category={transaction.category}
+                // Indicator color based on type
+                indicatorColor={
+                  transaction.type === "income" ? "var(--color-green)" : "red"
+                }
+                // Delete function based on type
+                deleteItem={
+                  transaction.type === "income" ? deleteIncome : deleteExpense
+                }
+              />
+            ))}
+          </div>
+        </div>
+      </InnerLayout>
+    </TransactionsStyled>
+  );
 }
 
 const TransactionsStyled = styled.div`
+  .transactions-content {
     display: flex;
-    overflow: auto;
-    .transactions-content {
-        display: flex;
-        gap: 2rem;
-        .transactions {
-            flex: 1;
-        }
-    }
+    flex-direction: column;
+    gap: 2rem;
+  }
+
+  .sorting {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin-bottom: 1rem;
+  }
+
+  .sorting select,
+  .sorting input {
+    padding: 0.5rem;
+    border: 1px solid var(--color-gray-light);
+    border-radius: 5px;
+    font-size: 0.9rem;
+  }
 `;
 
 export default Transactions;
